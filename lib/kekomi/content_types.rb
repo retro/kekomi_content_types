f72aa50b19fc13e8addd64f04c9a25282b8ebf5f
Mihael Konjevic <konjevic@gmail.com>
2012-09-02 15:38:17 +0200

    Add defined hook when content type is created

--- lib/kekomi/content_types.rb
+++ lib/kekomi/content_types.rb
@@ -23,6 +23,7 @@ class Kekomi
       klass.class_eval &block
       klass.before_save :serialize_fields
       klass.validates_presence_of klass.represented_with unless klass.represented_with.nil?
+      klass.defined if klass.respond_to? :defined
       Store.instance.content_types << klass unless Store.instance.content_types.map(&:to_s).include? klass.to_s
       klass
     end
